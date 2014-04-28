0304f4d952e4756de79a89fb50fee0b2716f68a1
David Ellis <d.f.ellis@ieee.org>
2014-04-28 15:12:31 -0700

    Import insanity, and actually connect to zookeeper and start getting data from the server, but still returns nonsense

--- kafka-spraynozzle.java
+++ kafka-spraynozzle.java
@@ -1,3 +1,16 @@
+import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.lang.Runnable;
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.consumer.KafkaStream;
+import kafka.message.Message;
+import kafka.message.MessageAndMetadata;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.entity.ContentType;
@@ -8,18 +21,40 @@ import org.apache.http.impl.client.HttpClientBuilder;
 class KafkaSpraynozzle {
     public static void main(String[] args) {
         String topic = args[0];
-        String url = args[1];
-        System.out.println("Listening to " + topic + " topic and redirecting to " + url + " (not really)");
-        HttpClient client = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(url);
-        post.setHeader("User-Agent", "KafkaSpraynozzle-0.0.1");
-        StringEntity fakeJsonEntity = new StringEntity("{\"hello\": \"world\"}", ContentType.APPLICATION_JSON);
-        post.setEntity(fakeJsonEntity);
-        try {
-            HttpResponse response = client.execute(post);
-            System.out.println("Response code: " + response.getStatusLine().getStatusCode());
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
+        final String url = args[1];
+        String zk = args[2];
+        System.out.println("Listening to " + topic + " topic from " + zk + " and redirecting to " + url + " (not really)");
+
+        final HttpClient client = HttpClientBuilder.create().build();
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("zk.connect", zk);
+        kafkaProps.put("zk.connectiontimeout.ms", "1000");
+        kafkaProps.put("groupid", "kafka_spraynozzle");
+        ConsumerConfig consumerConfig = new ConsumerConfig(kafkaProps);
+        ConsumerConnector consumerConnector = Consumer.createJavaConsumerConnector(consumerConfig);
+        HashMap<String, Integer> topicParallelism = new HashMap<String, Integer>();
+        topicParallelism.put(topic, 4);
+        Map<String, List<KafkaStream<Message>>> topicMessageStreams = consumerConnector.createMessageStreams(topicParallelism);
+        List<KafkaStream<Message>> streams = topicMessageStreams.get(topic);
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        for(final KafkaStream<Message> stream: streams) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    for(MessageAndMetadata msgAndMessageData: stream) {
+                        HttpPost post = new HttpPost(url);
+                        post.setHeader("User-Agent", "KafkaSpraynozzle-0.0.1");
+                        StringEntity fakeJsonEntity = new StringEntity("{\"hello\": \"world\"}", ContentType.APPLICATION_JSON);
+                        post.setEntity(fakeJsonEntity);
+                        try {
+                            HttpResponse response = client.execute(post);
+                            System.out.println("Response code: " + response.getStatusLine().getStatusCode());
+                        } catch (java.io.IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
         }
     }
 }
