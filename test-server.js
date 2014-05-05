ac0ad83ca03fd0a2cfada94a35e9527b058aa5b1
David Ellis <d.f.ellis@ieee.org>
2014-05-05 08:31:24 -0700

    Giving the test server more CPUs

--- test-server.js
+++ test-server.js
@@ -1,7 +1,14 @@
+var cluster = require('cluster');
 var http = require('http');
 
-http.createServer(function(req, res) {
-    console.log('Got a request!');
-    req.on('data', function(chunk) { console.log(chunk.toString()); });
-    req.on('end', res.end.bind(res, '{}'));
-}).listen(11235);
+if (cluster.isMaster) {
+    for(var i = 0; i < 8; i++) {
+        cluster.fork();
+    }
+} else {
+    http.createServer(function(req, res) {
+        console.log('Got a request!');
+        req.on('data', function(chunk) { console.log(chunk.toString()); });
+        req.on('end', res.end.bind(res, '{}'));
+    }).listen(11235);
+}
