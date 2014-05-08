392b379e3613de0a0ae200fd64734e04758e8600
David Ellis <d.f.ellis@ieee.org>
2014-05-07 22:18:52 -0700

    Major rearchitecting. Separating the kafka reading threads from the http posting threads (multiple http threads to each kafka reading thread possible/desireable)

--- kafka-spraynozzle.java
+++ kafka-spraynozzle.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ExecutorService;
 import java.lang.Runnable;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.javaapi.consumer.ConsumerConnector;
@@ -28,12 +29,11 @@ import org.apache.http.util.EntityUtils;
 class KafkaSpraynozzle {
     // Indentation good!
     public static void main(String[] args) {
-        // TODO: Make the threadCount an argument as soon as I write a good topic tester
-        // to determine the best threadCount for a given topic.
-        final Integer threadCount = Integer.parseInt(args[3]);
         String topic = args[0];
         final String url = args[1];
         String zk = args[2];
+        final Integer threadCount = Integer.parseInt(args[3]);
+        final Integer partitionCount = Integer.parseInt(args[4]);
         System.out.println("Listening to " + topic + " topic from " + zk + " and redirecting to " + url + " (not really)");
 
         // Kafka setup stuff
@@ -46,52 +46,72 @@ class KafkaSpraynozzle {
         ConsumerConfig consumerConfig = new ConsumerConfig(kafkaProps);
         ConsumerConnector consumerConnector = Consumer.createJavaConsumerConnector(consumerConfig);
         HashMap<String, Integer> topicParallelism = new HashMap<String, Integer>();
-        topicParallelism.put(topic, threadCount);
+        topicParallelism.put(topic, partitionCount);
         Map<String, List<KafkaStream<Message>>> topicMessageStreams = consumerConnector.createMessageStreams(topicParallelism);
         List<KafkaStream<Message>> streams = topicMessageStreams.get(topic);
-        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount+partitionCount);
 
         // Http Connection Pooling stuff
         final PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(200);
         cm.setDefaultMaxPerRoute(20);
 
+        final ConcurrentLinkedQueue<ByteArrayEntity> queue = new ConcurrentLinkedQueue<ByteArrayEntity>();
+
         // I'll admit this is pretty cool, but the syntax for `Runnable`s is weird
         for(final KafkaStream<Message> stream: streams) {
             executor.submit(new Runnable() {
                 public void run() {
+                    long threadId = Thread.currentThread().getId();
                     // Supposedly the HTTP Client is threadsafe, but lets not chance it, eh?
-                    System.out.println("Starting thread");
-                    CloseableHttpClient client = HttpClientBuilder.create().setConnectionManager(cm).build();
+                    System.out.println("Starting thread " + threadId);
                     for(MessageAndMetadata msgAndMetadata: stream) {
                         // There's no retry logic or anything like that, so the least I can do
                         // is log about incoming messages and the status code I get back from the server.
                         // Be sure to redirect stdout and stderr to files or your perf will tank.
                         System.out.println("Processing message");
-                        HttpPost post = new HttpPost(url);
-                        post.setHeader("User-Agent", "KafkaSpraynozzle-0.0.1");
-                        try {
-                            // Why the heck do I need to cast the message object back into a Message type?
-                            // `msgAndOffset` doesn't have this wart. :(
-                            ByteBuffer message = ((Message)msgAndMetadata.message()).payload();
-                            Integer messageLen = ((Message)msgAndMetadata.message()).payloadSize();
-                            Integer messageOffset = message.arrayOffset();
-                            ByteArrayEntity jsonEntity = new ByteArrayEntity(message.array(), messageOffset, messageLen, ContentType.APPLICATION_JSON);
-                            jsonEntity.setContentEncoding("UTF-8");
-                            post.setEntity(jsonEntity);
-                            // The fact that `CloseableHttpClient` and `CloseableHttpResponse` are separate types
-                            // confuses me, and led to one of the hardest to track bugs in earlier version of this code.
-                            CloseableHttpResponse response = client.execute(post);
-                            System.out.println("Response code: " + response.getStatusLine().getStatusCode());
-                            EntityUtils.consume(response.getEntity());
-                        } catch (java.io.UnsupportedEncodingException e) {
-                            // I love that the compiler made me catch these exceptions.
-                            // At least for now, though, retries are not in scope, so just be loud when this happens.
-                            System.out.println("Encoding issue");
-                            e.printStackTrace();
-                        } catch (java.io.IOException e) {
-                            System.out.println("IO issue");
-                            e.printStackTrace();
+                        // Why the heck do I need to cast the message object back into a Message type?
+                        // `msgAndOffset` doesn't have this wart. :(
+                        ByteBuffer message = ((Message)msgAndMetadata.message()).payload();
+                        Integer messageLen = ((Message)msgAndMetadata.message()).payloadSize();
+                        Integer messageOffset = message.arrayOffset();
+                        ByteArrayEntity jsonEntity = new ByteArrayEntity(message.array(), messageOffset, messageLen, ContentType.APPLICATION_JSON);
+                        jsonEntity.setContentEncoding("UTF-8");
+                        queue.add(jsonEntity);
+                        System.out.println("Enqueued for posting");
+                    }
+                }
+            });
+        }
+
+        for(int i = 0; i < threadCount; i++) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    long threadId = Thread.currentThread().getId();
+                    System.out.println("Starting thread " + threadId);
+                    CloseableHttpClient client = HttpClientBuilder.create().setConnectionManager(cm).build();
+                    while(true) {
+                        if(queue.peek() != null) {
+                            try {
+                                System.out.println("Posting message");
+                                HttpPost post = new HttpPost(url);
+                                post.setHeader("User-Agent", "KafkaSpraynozzle-0.0.1");
+                                ByteArrayEntity jsonEntity = queue.poll();
+                                post.setEntity(jsonEntity);
+                                CloseableHttpResponse response = client.execute(post);
+                                System.out.println("Response code: " + response.getStatusLine().getStatusCode());
+                                EntityUtils.consume(response.getEntity());
+                            } catch (java.io.IOException e) {
+                                System.out.println("IO issue");
+                                e.printStackTrace();
+                            }
+                        } else {
+                            try {
+                                Thread.sleep(250);
+                            } catch (java.lang.InterruptedException e) {
+                                System.out.println("Sleep issue!?");
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
