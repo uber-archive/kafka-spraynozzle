3875e149e76c5c7f287f8fce1273475037779bf4
David Ellis <d.f.ellis@ieee.org>
2014-05-14 10:30:14 -0700

    Make the help a bit more useful

--- KafkaArgs.java
+++ KafkaArgs.java
@@ -5,8 +5,8 @@ import com.lexicalscope.jewel.cli.Unparsed;
 @CommandLineInterface(application="kafka-spraynozzle")
 public interface KafkaArgs {
     @Unparsed(name="topic", description="The kafka topic to stream from") String getTopic();
-    @Option(shortName="u", description="The URL to post kafka messages to") String getUrl();
-    @Option(shortName="z", description="The Zookeeper instance to read from") String getZk();
+    @Option(shortName="u", description="The URL to post kafka messages to (in the form http(s)://server.address(:port)/url)") String getUrl();
+    @Option(shortName="z", description="The Zookeeper instance to read from (in the form server.address:port)") String getZk();
     @Option(shortName="n", description="The number of HTTP posting threads to use") int getThreadCount();
     @Option(shortName="p", defaultValue="1", description="The number of topic partitions (default: 1)") int getPartitionCount();
     @Option(shortName="h", helpRequest=true, description="Display this Help message") boolean getHelp();
