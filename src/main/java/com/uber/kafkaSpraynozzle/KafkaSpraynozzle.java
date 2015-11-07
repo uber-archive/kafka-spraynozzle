1045340530f8948e85a1c8b70205476d2fa7cd19
David Ellis <d.f.ellis@ieee.org>
2015-11-06 18:08:32 -0800

    It's not a List, it's an array

--- src/main/java/com/uber/kafkaSpraynozzle/KafkaSpraynozzle.java
+++ src/main/java/com/uber/kafkaSpraynozzle/KafkaSpraynozzle.java
@@ -109,7 +109,7 @@ class KafkaSpraynozzle {
         // Http Connection Pooling stuff
         final PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
         cm.setMaxTotal(threadCount);
-        cm.setDefaultMaxPerRoute(threadCount / topics.size());
+        cm.setDefaultMaxPerRoute(threadCount / topics.length);
 
         // Message-passing queues within the spraynozzle
         final ConcurrentLinkedQueue<ByteArrayEntity> queue = new ConcurrentLinkedQueue<ByteArrayEntity>();
